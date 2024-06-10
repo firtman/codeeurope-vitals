175cdf2ad7c3583a510d9933313abe5a67e08c66
Maximiliano Firtman <firtman@gmail.com>
2024-06-11 00:53:19 +0200

    fake pause larger

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use((req, res, next) => {
     // Fake pause
-    setTimeout(next, Math.floor( ( Math.random() * 1500 ) + 100 ) );
+    setTimeout(next, Math.floor( ( Math.random() * 2500 ) + 100 ) );
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
