e5fa0de8469064f89845780e9ad195a8851f6879
Maximiliano Firtman <firtman@gmail.com>
2024-06-11 00:49:13 +0200

    inp test added

--- public/script.js
+++ public/script.js
@@ -2,16 +2,21 @@
 async function load() {
     const response = await fetch("gallery/data.json");
     const data = await response.json();
-    data.forEach(exhibition => {
+    data.forEach( exhibition => {
         const article = `
             <article>
                 <h2>${exhibition.title}</h2>
                 <p>${exhibition.description}</p>
-                <img src="gallery/${exhibition.image}">
+                <img onclick="fullscreen(event)" src="gallery/${exhibition.image}">
             </article>
         `;
         document.querySelector("main").innerHTML += article;
     });
 }
+
+async function fullscreen(event) {
+    setTimeout(() => event.target.requestFullscreen(), 
+            Math.floor(Math.random() * 2500));
+};
 
 window.onload = load;
